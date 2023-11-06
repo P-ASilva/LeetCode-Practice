589c8248c17b5f547c043cd9aac9ddc9f579d8d0
Pedro Antônio Silva <105285216+P-ASilva@users.noreply.github.com>
2023-11-06 17:33:25 -0300

    Time: 11 ms (32.08%), Space: 7.2 MB (78.30%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.c
+++ 0841-keys-and-rooms/0841-keys-and-rooms.c
@@ -1,9 +1,9 @@
 void visitAccessibleRooms(bool* accessible, int current, int** rooms, int* roomsColSize) {
-    if (!accessible[current]) {
-            accessible[current] = true;
-        for (int i = 0; i < roomsColSize[current]; i++ ) {  
+    accessible[current] = true;
+    for (int i = 0; i < roomsColSize[current]; i++ ) {  
+        if (!accessible[rooms[current][i]]) {
             visitAccessibleRooms(accessible, rooms[current][i], rooms,  roomsColSize);
-        }
+        }  
     }
 }
 
